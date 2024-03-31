454578dbb2e7c7fac67f8808fd1d3e6e97e7ffc2
mahdi1297 <mahdi9193514418@gmail.com>
2024-04-01 01:33:52 +0330

    header and sidebar layout implementd

--- src/layouts/main-layout/header.tsx
+++ src/layouts/main-layout/header.tsx
@@ -1,5 +1,9 @@
 import './style.scss'
 
 export const Header = () => {
-    return <header className='header w-100 pos-fixed'></header>
+    return <header className='header w-100 pos-fixed pr-24'>
+        <div className='w-100 h-100 d-flex justify-between align-center'>
+            
+        </div>
+    </header>
 }
